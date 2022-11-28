94ec8e59187644d6ef635d4231cce42556c3b468
tttpm <119104400+tttpm@users.noreply.github.com>
2022-11-28 19:05:08 +0500

    yeah

--- CSETR.py
+++ CSETR.py
@@ -11,6 +11,10 @@ import ImgConv #yeah
 
 colorama.init()
 
+picture = [[None] * 20 for i in range (18)]
+
+pic_size = (len(picture), len(picture[0]))
+
 #################################################################################################################################################################################################
 def color(r,g,b, fore = True, back = False):
     
@@ -22,7 +26,7 @@ def color(r,g,b, fore = True, back = False):
 def decode_hex(hexx):
     if len(hexx) == 8:
         if hexx[-2:] == '00':
-            return a
+            return None
     hexx = hexx.upper()
     digits = '0123456789ABCDEF'
     cat = list(map(lambda d: digits.index(d), hexx[:]))
@@ -73,33 +77,70 @@ def skin_to_png(skin):
     return img
 
 
+def import_skin(trskin):
+    res = [[None]*pic_size[1] for i in range(pic_size[0])]
+    compressed = base64.b64decode(trskin[7:])
+    hexes = str(zlib.decompress(compressed, -zlib.MAX_WBITS).decode()).split(';')
+    hexes.pop()
+    i, j = 0, 0
+    for hehex in hexes:
+        if j == pic_size[1]:
+            i += 1
+            j = 0
+        res[i][j] = decode_hex(hehex)
+        j += 1
+    return res
+
+
+def export_skin(pic):
+    res = ''
+    for line in pic:
+        for pixel in line:
+            if not pixel:
+                res += '00000000;'
+                continue
+            res += encode_hex(pixel) + ';'
+            
+    if not res: return 'trSkin1'
+    deflate_compress = zlib.compressobj(9, zlib.DEFLATED, -zlib.MAX_WBITS)
+    compressed = deflate_compress.compress(bytes(res, encoding ='utf-8')) + deflate_compress.flush()
+    encrypted = base64.b64encode(compressed)
+    return "trSkin1" + encrypted.decode()
+
+
+
 ##################################################################################################################################################################################################
 clear_color = '\033[0m'
+
 
 try:
     with open("CSETR_config.txt") as file:
         text = file.read().split('\n')
     text.pop()
-    bgc_hex = text[0] #background color hex
-    bgcf = color(*decode_hex(bgc_hex))
-    bgcb = color(*decode_hex(bgc_hex), back = True, fore = False)
     
-    defhex = text[1] #'default' color hex
-    default = color(*decode_hex(defhex))
+    bgc_hex, def_hex, bg_skin = text
+    
     
 except:
+    
     bgc_hex = "0c0c0c"
-    bgcf = color(*decode_hex(bgc_hex))
-    bgcb = color(*decode_hex(bgc_hex), back = True, fore = False)
-
     defhex = 'cccccc'
-    default = color(*decode_hex(defhex))
+    bg_skin = 'trSkin1MzCAAGuDUcYAMRwNQNDNDcFwgwJ6MIaQU8lz/Eh2s6OriamlCzKDPDWjbh5182Bw82i5gd/NZi5GTmZGQMVwBmYM0pMx6uZRN1Pi5kHieOo3+Sizi55OHSRuBgA='
+
+
+    
+bgcf = color(*decode_hex(bgc_hex))
+bgcb = color(*decode_hex(bgc_hex), back = True, fore = False)
+default = color(*decode_hex(defhex))
+bg = import_skin(bg_skin)
+
+    
 
 def save_config():
     with open("CSETR_config.txt", 'w') as file:
         print(bgc_hex,
               defhex,
-
+              bg_skin,
               
               file = file, sep = '\n')
 
@@ -109,33 +150,6 @@ current_color = (255,255,255)
 mode = 'brush'
 suncat = 'trSkin17ZZBCoAwDAS/VFCk4qmt5v9P8iBIsTVuYqwIelq0ZidhJTq3XZODBdFRiF43r8NbLF0InnKhMLWqA1rMQ1pSzMWZaVUo6hiiMqaMENVRwEu9rERL5jIJusM/8yeYd5F8H8aYi+eYy0ci5hIVgWcImTNlZRy+Ol6kC4YQuXPJjGxPEBVJCzJwxXjV2RCFpEE29k/YnJBZJVbZQPayuVBkg+mrpbj/U0r0gmgAf7NBxH0F'
 b, w, g, a = (174,69,157), (255,255,255), (160, 160, 160), None
-
-bg = [
-
-[a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a], 
-[a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a], 
-[a, a, a, g, g, w, w, w, w, w, w, w, w, w, w, w, w, a, a, a],
-[a, a, g, g, w, w, w, w, w, w, w, w, w, w, w, w, w, w, a, a],
-[a, a, g, g, w, w, w, w, w, w, w, w, w, w, w, w, w, w, a, a],
-[a, a, g, g, w, w, w, w, w, w, b, b, w, w, b, b, w, w, a, a],
-[a, a, g, g, w, w, w, w, w, w, b, b, w, w, b, b, w, w, a, a],
-[a, a, g, g, w, w, w, w, w, w, b, b, w, w, b, b, w, w, a, a],
-[a, a, g, g, w, w, w, w, w, w, b, b, w, w, b, b, w, w, a, a],
-[a, a, g, g, w, w, w, w, w, w, w, w, w, w, w, w, w, w, a, a],
-[a, a, g, g, w, w, w, w, w, w, w, w, w, w, w, w, w, w, a, a],
-[a, a, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, a, a],
-[a, a, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, a, a],
-[a, a, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, a, a],
-[a, a, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, a, a],
-[a, a, a, b, b, b, b, b, b, b, b, b, b, b, b, b, b, a, a, a],
-[a, a, a, a, a, a, b, b, b, a, a, a, a, b, b, b, a, a, a, a],
-[a, a, a, a, a, a, b, b, b, a, a, a, a, b, b, b, a, a, a, a]
-
-]
-
-picture = [[a] * 20 for i in range (18)]
-
-pic_size = (len(picture), len(picture[0]))
 
 
 
@@ -166,36 +180,7 @@ def print_pic():
         
         print(default + "#")
     print(default + '#'*62)
-
-
-        
-def export_skin():
-    res = ''
-    for line in picture:
-        for pixel in line:
-            if not pixel:
-                res += '00000000;'
-                continue
-            res += encode_hex(pixel) + ';'
-            
-    if not res: return 'trSkin1'
-    deflate_compress = zlib.compressobj(9, zlib.DEFLATED, -zlib.MAX_WBITS)
-    compressed = deflate_compress.compress(bytes(res, encoding ='utf-8')) + deflate_compress.flush()
-    encrypted = base64.b64encode(compressed)
-    return "trSkin1" + encrypted.decode()
-
-def import_skin(trskin):
-    compressed = base64.b64decode(trskin[7:])
-    hexes = str(zlib.decompress(compressed, -zlib.MAX_WBITS).decode()).split(';')
-    hexes.pop()
-    i, j = 0, 0
-    for hehex in hexes:
-        if j == pic_size[1]:
-            i += 1
-            j = 0
-        picture[i][j] = decode_hex(hehex)
-        j += 1
-    
+ 
 
 def fill():
     x, y = selected
@@ -238,9 +223,9 @@ def import_png(img):
     for y in range(0, img.size[1]):
         for x in range(0, img.size[0]):
             
-            r,g,b,a = pixels[x, y]
+            r,g,b,alpha = pixels[x, y]
 
-            if a < 100:
+            if alpha < 100:
                 picture[y][x] = a
             else:
                 picture[y][x] = (r,g,b)
@@ -250,7 +235,7 @@ def import_png(img):
 ##################################################################################################################################################################################################
 
 
-x = (0,0,0)
+w, x = (255,255,255),(0,0,0)
 logo = [
 
 [w, w, w, w, w, w, w, w, w],
@@ -284,9 +269,8 @@ def print_logo():
 
 print_logo()
 print(default+'Console Skin Editor for Team Run (by '+color(255,102,0, back=False)+'Tipim'+default+')')
-print('press [esc] to start')
-kb.wait('esc')
-
+print('press [alt] to start')
+kb.wait('alt')
 while 1:
     try:
         system("cls")
@@ -332,7 +316,7 @@ while 1:
                 
             elif action == 'e':
                 print('your skin:\n')
-                text = export_skin()
+                text = export_skin(picture)
                 print(text)
                 bebra = input('\nwrite "png" to save skin as .PNG file;\nwrite "otrmap" to save skin as .OTRMAP file\nwrite something else to continue\n>> ')
 
@@ -354,13 +338,13 @@ while 1:
                     kb.wait("esc")
 
             elif action == 'i':
-                amogum = input('\nLoad skin from...\n"t" - text\n"i" - image (PNG)\n>> ')
+                amogum = input('\nLoad skin from...\n"t" - text\n"i" - image (PNG)\n"b" - background\n>> ')
                 if amogum == 't':
                     inp = input('enter your skin:\n>> ')
                     if inp.lower() in ['suncat', 'tdf', 'солнцекот', 'cjkywtrjn']:
-                        import_skin(suncat)
+                        picture = import_skin(suncat)[::]
                     else:
-                        import_skin(inp)
+                        picture = import_skin(inp)[::]
                     print('press [esc] to continue')
                     kb.wait("esc")
                     continue
@@ -380,6 +364,10 @@ while 1:
                     
                     print('done!\npress [esc] to continue')
                     kb.wait("esc")
+                elif amogum == "b":
+                    picture = bg[::]
+                    print('done!\npress [esc] to continue')
+                    kb.wait("esc")
 
             elif action == 'cu':
                 print(f'''
@@ -387,14 +375,20 @@ while 1:
 current parameters:
 background color - #{bgcf}{bgc_hex}{default}(default - #0c0c0c)
 UI color - #{defhex} (default - #cccccc)
+background skin - {bg_skin}
 ''')
-
-                bgc_hex = input("new background color:\n>> ").replace('#','')
-                bgcb = color(*decode_hex(bgc_hex), back  = True, fore = False)
-                bgcf = color(*decode_hex(bgc_hex))
-
-                defhex = input("new UI color:\n>> ").replace('#','')
-                default = color(*decode_hex(defhex))
+                act = input('\n"bc" - change background color\n"bs" - change background skin\n"ui" - change UI color\n>> ')
+                if act == 'bc':
+                    bgc_hex = input("new background color:\n>> ").replace('#','')
+                    bgcb = color(*decode_hex(bgc_hex), back  = True, fore = False)
+                    bgcf = color(*decode_hex(bgc_hex))
+                elif act == 'ui':
+                    defhex = input("new UI color:\n>> ").replace('#','')
+                    default = color(*decode_hex(defhex))
+                elif act == 'bs':
+                    bg_skin = input("new background skin:\n>> ")
+                    bg = import_skin(bg_skin)
+                    
                 
                 save_config()
                 
